9768db456a43b426b25873b6e4d9ac6d53e74c2e
naimur1046 <naimurrahman1046@gmail.com>
2026-03-13 15:49:23 +0600

    Add login and register dto

---
+++ api/EMS.Application/DTOs/Auth/RegisterDto.cs
@@ -0,0 +1,8 @@
+namespace EMS.Application.DTOs.Auth;
+
+public class RegisterDto
+{
+    public string Email { get; set; }
+    public string Password { get; set; } 
+    public string Name { get; set; }
+}
