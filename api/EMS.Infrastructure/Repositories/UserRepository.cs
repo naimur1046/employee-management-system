7d29fcfd2c985ee810a92813a194bc30e2a5ed08
naimur1046 <naimurrahman1046@gmail.com>
2026-03-24 20:10:26 +0600

    Add user repository

---
+++ api/EMS.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+using EMS.Domain.Entities;
+using EMS.Domain.Interfaces;
+using EMS.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+    }
+}
