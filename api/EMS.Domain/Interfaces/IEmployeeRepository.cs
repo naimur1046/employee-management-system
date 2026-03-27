cf75bc49071207a077e4fb96c598eb0bbe2c7916
naimur1046 <naimurrahman1046@gmail.com>
2026-03-27 21:14:35 +0600

    Update employee repository for the get pagged employees

--- api/EMS.Domain/Interfaces/IEmployeeRepository.cs
+++ api/EMS.Domain/Interfaces/IEmployeeRepository.cs
@@ -5,6 +5,7 @@ namespace EMS.Domain.Interfaces;
 public interface IEmployeeRepository
 {
     Task<IEnumerable<Employee>> GetAllAsync();
+    Task<(IEnumerable<Employee> Items, int TotalCount)> GetPagedAsync(int pageNumber, int pageSize);
     Task<Employee?> GetByIdAsync(Guid id);
     Task<Employee> AddAsync(Employee employee);
     Task UpdateAsync(Employee employee);
