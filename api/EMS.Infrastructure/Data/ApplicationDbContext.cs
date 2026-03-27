63113d1d945531ad76a745cd7ad06ecfed58a7bc
naimur1046 <naimurrahman1046@gmail.com>
2026-03-27 11:10:28 +0600

    update ApplicationDbContext for the employees

--- api/EMS.Infrastructure/Data/ApplicationDbContext.cs
+++ api/EMS.Infrastructure/Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<User> Users { get; set; }
+    public DbSet<Employee> Employees { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -21,7 +22,24 @@ public class ApplicationDbContext : DbContext
             entity.HasIndex(e => e.Email).IsUnique();
             entity.Property(e => e.Password).IsRequired();
         });
-        
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.FullName).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.Department).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.ContactNumber).HasMaxLength(20);
+            entity.Property(e => e.Organization).HasMaxLength(100);
+            entity.Property(e => e.Branch).HasMaxLength(100);
+            entity.Property(e => e.Campus).HasMaxLength(100);
+            entity.Property(e => e.BloodGroup).HasMaxLength(10);
+            entity.Property(e => e.OfficeEmail).HasMaxLength(100);
+            entity.HasIndex(e => e.OfficeEmail).IsUnique();
+            entity.Property(e => e.Pin).HasMaxLength(50);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Designation).HasMaxLength(100);
+        });
+
         base.OnModelCreating(modelBuilder);
     }
 }
