e26460f68ddd9bfabb7ae3c0e78c04270e30baea
naimur1046 <naimurrahman1046@gmail.com>
2026-03-27 11:10:06 +0600

    Add admin and employee routes in the app.routes fils

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,6 +6,15 @@ export const routes: Routes = [
     loadChildren: () => import('./features/auth/auth-module').then(m => m.AuthModule),
   },
   {
+    path: 'admin',
+    loadChildren: () => import('./features/admin/admin-routing').then(m => m.adminRoutes),
+  },
+  {
+    path: 'employees',
+    loadComponent: () => import('./features/employees/components/manage/manage-employee.component')
+      .then(m => m.ManageEmployeeComponent),
+  },
+  {
     path: '',
     redirectTo: 'auth/login',
     pathMatch: 'full',
