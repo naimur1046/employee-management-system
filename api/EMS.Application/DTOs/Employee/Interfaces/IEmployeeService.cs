539f26899369718f24c043e7f2e9e7444adeec92
naimur1046 <naimurrahman1046@gmail.com>
2026-03-27 21:15:01 +0600

    update employee service for get paged employee

--- api/EMS.Application/DTOs/Employee/Interfaces/IEmployeeService.cs
+++ api/EMS.Application/DTOs/Employee/Interfaces/IEmployeeService.cs
@@ -1,3 +1,4 @@
+using EMS.Application.Common;
 using EMS.Application.DTOs.Employee;
 
 namespace EMS.Application.Interfaces;
@@ -5,6 +6,7 @@ namespace EMS.Application.Interfaces;
 public interface IEmployeeService
 {
     Task<IEnumerable<EmployeeResponseDto>> GetAllAsync();
+    Task<EmployeePagedResponseDto> GetPagedAsync(int pageNumber, int pageSize);
     Task<EmployeeResponseDto> GetByIdAsync(Guid id);
     Task<EmployeeResponseDto> CreateAsync(CreateEmployeeDto dto);
     Task<EmployeeResponseDto> UpdateAsync(Guid id, UpdateEmployeeDto dto);
