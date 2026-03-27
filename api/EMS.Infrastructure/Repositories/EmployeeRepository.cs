601e941520fd05fbc956a056c60688e3884d91bd
naimur1046 <naimurrahman1046@gmail.com>
2026-03-27 10:46:37 +0600

    add employee repository implementation

---
+++ api/EMS.Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,59 @@
+using EMS.Domain.Entities;
+using EMS.Domain.Interfaces;
+using EMS.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.Infrastructure.Repositories;
+
+public class EmployeeRepository : IEmployeeRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public EmployeeRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Employee>> GetAllAsync()
+    {
+        return await _context.Employees
+            .Where(e => !e.IsDeleted)
+            .ToListAsync();
+    }
+
+    public async Task<Employee?> GetByIdAsync(Guid id)
+    {
+        return await _context.Employees
+            .FirstOrDefaultAsync(e => e.Id == id && !e.IsDeleted);
+    }
+
+    public async Task<Employee> AddAsync(Employee employee)
+    {
+        await _context.Employees.AddAsync(employee);
+        await _context.SaveChangesAsync();
+        return employee;
+    }
+
+    public async Task UpdateAsync(Employee employee)
+    {
+        _context.Employees.Update(employee);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var employee = await _context.Employees.FindAsync(id);
+        if (employee != null)
+        {
+            employee.IsDeleted = true;
+            employee.DeletedOnUtc = DateTime.UtcNow;
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<Employee?> GetByEmailAsync(string email)
+    {
+        return await _context.Employees
+            .FirstOrDefaultAsync(e => e.OfficeEmail == email && !e.IsDeleted);
+    }
+}
